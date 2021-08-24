2aa5d25ba74fc83143ff22c18de2ef6f2dbc07de
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-23 19:23:50 -0500

    Add pages and Dinamic Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,48 @@ import ListImages from "./Components/ListImages";
 function App() {
   const [search, setSearch] = useState("");
   const [images, setImages] = useState([])
+  const [paginaActual, setPaginaActual] = useState(1)
+  const [totalPaginas, setTotalPaginas] = useState(1)
 
   useEffect(() => {
     const apiPixebay = async () => {
       if (search === "") return;
 
       const imagesPerPage = 20;
-      const URL = `https://pixabay.com/api/?key=23031099-d0b3f2b93f3eaf77f8fa3497e&q=${search}&per_page=${imagesPerPage}`;
+      const URL = `https://pixabay.com/api/?key=23031099-d0b3f2b93f3eaf77f8fa3497e&q=${search}
+      &per_page=${imagesPerPage}&page=${paginaActual}`;
 
       const response = await fetch(URL);
       const result = await response.json();
       setImages(result.hits)
+      console.log(result)
+
+     const totalPages = Math.ceil(result.totalHits / imagesPerPage )
+     setTotalPaginas(totalPages)
     };
 
+    const jumbotron = document.querySelector('.jumbotron')
+    jumbotron.scrollIntoView({ behavior: 'smooth'})
+
     apiPixebay();
-  }, [search]);
+  }, [search, paginaActual]);
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual -1
+
+     if(nuevaPaginaActual === 0) return
+     setPaginaActual(nuevaPaginaActual)
+     console.log(nuevaPaginaActual)
+  }
+
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaActual +1
+
+    if(nuevaPaginaActual === totalPaginas + 1) return
+    setPaginaActual(nuevaPaginaActual)
+    console.log(nuevaPaginaActual)
+
+  }
 
   return (
     <div className="container">
@@ -29,7 +56,27 @@ function App() {
       </div>
       <div className="row justify-content-center">
         <ListImages images={images} />
-      </div>
+
+      {
+        paginaActual === 1 ? null : 
+        ( <button
+          className="btn btn-info mr-1"
+          onClick={paginaAnterior}>
+            Anterior &laquo;
+          </button> )
+      }
+
+
+      {
+        paginaActual === totalPaginas ? null : 
+        ( <button
+          className="btn btn-info mr-1"
+          onClick={paginaSiguiente}>
+            Siguiente &raquo;
+          </button>)
+      }
+
+      </div>       
     </div>
   );
 }
--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Image = ({image}) => {
 
-    const {largeImageURL, likes, previewURL, tags, views, type} = image
+    const {largeImageURL, likes, previewURL, views, type} = image
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
            <div className="card" >
@@ -11,6 +11,25 @@ const Image = ({image}) => {
                 alt={type}
                 className="card-img-top"
                 />
+
+                <div className="card-body">
+                   <p className="card-text">
+                       {likes} Me Gusta
+                   </p>
+                   <p className="card-text">
+                       {views} Vistas
+                   </p>
+                </div>
+
+                <div className="card-footer">
+                    <a 
+                    href={largeImageURL}
+                    target="blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-primary btn-block center"
+                    >Ver imagen</a>
+
+                </div>
 
            </div>
         </div>
